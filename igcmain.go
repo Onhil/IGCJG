5971718e544ec8dc8f01a35f0763cff8e217a842
Onhil <onhilx@gmail.com>
2018-10-01 18:15:55 +0200

    some changes

---
+++ igcmain.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func apiUptime(w http.ResponseWriter, r *http.Request) {
+	http.Header.Add(w.Header(), "content-type", "application/json")
+	apiMessage := MetaInfo(Uptime, "Service for IGC tracks.", "v1")
+	b, err := json.Marshal(apiMessage)
+
+}
+
+func main() {
+	http.HandleFunc("/igcinfo/api", APIUptime)
+	err := http.ListenAndServe(":8080", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
---
+++ metaInfoAPI.go
@@ -0,0 +1,14 @@
+package main
+
+// MetaInfo struct for api uptime and meta info
+type MetaInfo struct {
+	uptime  string
+	info    string
+	version string
+}
+
+// Uptime returns app uptime in the ISO 8601 standard
+func Uptime() string {
+	sysinfo
+
+}
